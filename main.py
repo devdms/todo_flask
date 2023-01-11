0c8c57675ac9460f8590f2b1e2d6050de373050e
devdms <danmartins.dg@gmail.com>
2023-01-11 14:59:22 -0300

    Mudanças a serem submetidas: 	modified:   main.py 	modified:   static/style.css 	modified:   tasks.db 	modified:   templates/show_tasks.html

--- main.py
+++ main.py
@@ -1,70 +1,110 @@
 from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
+from sqlite3 import Error
 
-static_folder = 'static'
-app=Flask(__name__,template_folder='templates')
+app = Flask(__name__,template_folder='templates',static_folder='static')
 
-# Conecta ao banco de dados
+# Connect to the database
 def connect_db():
-    return sqlite3.connect('tasks.db')
+    try:
+        conn = sqlite3.connect('tasks.db')
+        return conn
+    except Error as e:
+        print(e)
 
-# Exibe a lista de tarefas
+# Show the list of tasks
 @app.route('/')
 def show_tasks():
-    # Conecta ao banco de dados
+    # Connect to the database
     conn = connect_db()
     c = conn.cursor()
 
-    # Seleciona todas as tarefas pendentes do banco de dados
+    # Select all pending tasks from the database
     c.execute("SELECT * FROM tasks WHERE status='pending'")
     tasks = c.fetchall()
 
-    # Fecha a conexão com o banco de dados
+    # Close the database connection
     conn.close()
 
-    # Renderiza a página HTML com a lista de tarefas
+    # Render the HTML page with the list of tasks
     return render_template('show_tasks.html', tasks=tasks)
 
-# Adiciona uma nova tarefa
+# Add a new task
 @app.route('/add_task', methods=['POST'])
 def add_task():
-    # Obtém os dados do formulário
-    task_name = request.form['task_name']
+    # Get the data from the form
+    task_name = request.form.get('task_name', None)
 
-    # Conecta ao banco de dados
+    # Connect to the database
     conn = connect_db()
     c = conn.cursor()
 
-    # Insere a nova tarefa no banco de dados
-    c.execute("INSERT INTO tasks (task_name, status) VALUES (?, 'pending')", (task_name,))
+    # Input validation 
+    if task_name and task_name.strip():
+        # Insert the new task into the database
+        c.execute("INSERT INTO tasks (task_name, status) VALUES (?, 'pending')", (task_name,))
 
-    # Salva as alterações no banco de dados
-    conn.commit()
+        # Save changes to the database
+        conn.commit()
+    else:
+        # Return error message if task name is empty
+        return "Task name is required", 400
 
-    # Fecha a conexão com o banco de dados
+    # Close the database connection
     conn.close()
 
-    # Redireciona o usuário para a página principal
+    # Redirect the user to the main page
     return redirect(url_for('show_tasks'))
 
-# Marca uma tarefa como concluída
-@app.route('/complete_task/<task_id>')
+# Mark a task as completed
+@app.route('/complete_task/<int:task_id>')
 def complete_task(task_id):
-    # Conecta ao banco de dados
+    # Connect to the database
     conn = connect_db()
     c = conn.cursor()
 
-    # Atualiza o status da tarefa para "concluída" no banco de dados
-    c.execute("UPDATE tasks SET status='completed' WHERE task_id=?", (task_id,))
+    # Input validation
+    if task_id:
+        # Update the task status to "completed" in the database
+        c.execute("UPDATE tasks SET status='completed' WHERE task_id=?", (task_id,))
 
-    # Salva as alterações no banco de dados
-    conn.commit()
+        # Save changes to the database
+        conn.commit()
+    else:
+        # Return error message if task_id is not provided
+        return "Task id is required", 400
 
-    # Fecha a conexão com o banco de dados
+    # Close the database connection
     conn.close()
 
-    # Redireciona o usuário para a página principal
+    # Redirect the user to the main page
     return redirect(url_for('show_tasks'))
+
+#Export tasks to a text file
+@app.route('/export_tasks')
+def export_tasks():
+    # Connect to the database
+    conn = connect_db()
+    c = conn.cursor()
+
+    # Select all tasks from the database
+    c.execute("SELECT task_name FROM tasks")
+    tasks = c.fetchall()
+
+    # Close the database connection
+    conn.close()
+
+    # Create the text file
+    task_file = '\n'.join([task[0] for task in tasks])
+
+    # Create a response object with the text file
+    response = make_response(task_file)
+
+    # Set the headers to indicate that this is a text file
+    response.headers["Content-Disposition"] = "attachment; filename=tasks.txt"
+    response.headers["Content-type"] = "text/plain"
+
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True)
